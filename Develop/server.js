fbd6d080da73b9bc63b54172b9ab5e6b7aa0a688
Malachi Chapman <mchapm17@outlook.com>
2021-12-31 20:39:23 -0500

    updated server

--- Develop/server.js
+++ Develop/server.js
@@ -22,11 +22,20 @@ app.get('/api/notes', (req, res) => {
 
 app.post('/api/notes', (req, res) => {
   const data = fs.readFileSync('./db/db.json', 'utf8');
-  const notes = JSON.parse(data)
+  const notes = JSON.parse(data);
+  req.body.id = uuidv4();
   notes.push(req.body);
   const newNotes = JSON.stringify(notes, null, 2);
   fs.writeFileSync('./db/db.json', newNotes);
-  res.json(notes);
+  res.json(req.body);
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  const { id } = req.params;
+  const data = fs.readFileSync('./db/db.json', 'utf8');
+  const notes = JSON.parse(data).filter(n => n.id !== id);
+  const newNotes = JSON.stringify(notes, null, 2);
+  fs.writeFileSync('./db/db.json', newNotes);
 });
 
 app.listen(PORT, ()  => console.log('listening on http://localhost:' + PORT));
